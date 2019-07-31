fbe4f98abdacdfeb5f925f675b796ce0335aa3a1
Francisco Javier Mendoza <pacopancho413@gmail.com>
2019-07-31 13:22:01 -0500

    Implementando los método del modelo ProductController

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -2,84 +2,22 @@
 
 namespace App\Http\Controllers\Product;
 
+use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\APIController;
 
 class ProductController extends APIController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    //Función para mostrar todas las instancias de Product
     public function index()
     {
-        //
-    }
+        $products = Product::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return $this->showAll($products);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    //Funcion para retornar la instancia
+    public function show(Product $product)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return $this->showOne($product);
     }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,16 +6,18 @@ use App\Seller;
 use App\Category;
 use App\Transaction;
 use Illuminate\Database\Eloquent\Model;
+use App\Transformers\ProductTransformer;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-	//Constantes para el estado del producto
+    use SoftDeletes;
+
 	const PRODUCTO_DISPONIBLE = 'disponible';
 	const PRODUCTO_NO_DISPONIBLE = 'no disponible';
 
-    //Indicando que el campo es para una fecha
+    public $transformer = ProductTransformer::class;
     protected $dates = ['deleted_at'];
-
     protected $fillable = [
     	'name',
     	'description',
@@ -24,24 +26,28 @@ class Product extends Model
     	'image',
     	'seller_id',
     ];
+    protected $hidden = [
+        'pivot'
+    ];
 
-    //Función para verificar el estado del producto
-    public function estaDisponible() {
+    public function estaDisponible()
+    {
     	return $this->status == Product::PRODUCTO_DISPONIBLE;
     }
 
-    //Función para crear relacion con el modelo Category
-    public function categories() {
-        return $this->belongsToMany(Category::class);
-    }
-
-    //función para la relación con el modelo Seller
-    public function seller() {
+    public function seller()
+    {
         return $this->belongsTo(Seller::class);
     }
 
-    //Función para la relación con el modelo Transaction
-    public function transactions() {
+    public function transactions()
+    {
         return $this->hasMany(Transaction::class);
+    }
+
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
     }
 }
