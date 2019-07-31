414710a50b0bdbd70d0669aea2ff04f7ea2894b6
Francisco Javier Mendoza <pacopancho413@gmail.com>
2019-07-31 11:15:16 -0500

    Global Scope para el modelo Buyer

--- app/Buyer.php
+++ app/Buyer.php
@@ -3,10 +3,19 @@
 namespace App;
 
 use App\Transaction;
+use App\Scopes\BuyerScope;
 
 
 class Buyer extends User
 {
+	//Funcion ejecutada al inicio del modelo
+	//para ejecutar el scope
+	protected static function boot()
+	{
+		parent::boot();
+
+		static::addGlobalScope(new BuyerScope);
+	}
     //Función para la relacion con la tabla transactions 
     public function transactions() {
     	return $this->hasMany(Transaction::class);
---
+++ app/Scopes/BuyerScope.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Builder;
+
+class BuyerScope implements Scope
+{
+	//Aplica solo si se tienen transacciones
+	public function apply(Builder $builder, Model $model)
+	{
+		$builder->has('transactions');
+	}
+}
