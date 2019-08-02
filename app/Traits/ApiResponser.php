64e6caa7baa99d945339087faa4642d07259b9af
Francisco Javier Mendoza <pacopancho413@gmail.com>
2019-08-02 13:02:40 -0500

    Resolviendo problemas de paginación con caché

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -123,7 +123,15 @@ trait ApiResponser
 	{
 		$url = request()->url();
 
-		return Cache::remember($url, 30/60, function() use ($data){
+		$queryParams = request()->query();
+
+		ksort($queryParams);
+
+		$queryString = http_build_query($queryParams);
+
+		$fullUrl = "{$url}?{$queryString}";
+
+		return Cache::remember($fullUrl, 30/60, function() use ($data){
 			return $data;
 		});
 	}
